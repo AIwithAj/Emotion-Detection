f2f43aedf554f659fd3a384812255fbbea14a65e
Ajay <dancerworld60@gmail.com>
2024-05-01 13:47:17 +0530

    Base model is Ready

---
+++ src/Emotion_Detection/components/prepare_base_model.py
@@ -0,0 +1,72 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+from src.Emotion_Detection.entity.config_entity import PrepareBaseModelConfig
+from pathlib import Path
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization, Dropout, Flatten, Dense
+
+
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
+        self.config = config
+
+    
+    def get_base_model(self):
+        self.model =tf.keras.applications.ResNet50V2(input_shape=self.config.params_image_size,
+                                               include_top= self.config.params_include_top,
+                                               weights=self.config.params_weights
+                                               )
+        self.save_model(path=self.config.base_model_path, model=self.model)
+
+
+    
+    @staticmethod
+    def _prepare_full_model(model, classes, freeze_all, freeze_till):
+        if freeze_all:
+            for layer in model.layers:
+                layer.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                layer.trainable = False
+
+        full_model = Sequential([
+                      model,
+                      Dropout(0.25),
+                      BatchNormalization(),
+                      Flatten(),
+                      Dense(64, activation='relu'),
+                      BatchNormalization(),
+                      Dropout(0.5),
+                      Dense(classes,activation='softmax')
+                    ])
+        print('befor compilation')
+        full_model.summary()
+        
+        full_model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
+    
+
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=False,
+            freeze_till=50,
+           
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+    
+
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
--- src/Emotion_Detection/config/configuration.py
+++ src/Emotion_Detection/config/configuration.py
@@ -1,6 +1,8 @@
 from src.Emotion_Detection.constants import *
 from src.Emotion_Detection.utils.common import read_yaml,create_directories
-from src.Emotion_Detection.entity.config_entity import DataIngestionConfig
+from src.Emotion_Detection.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
+from src.Emotion_Detection.constants import *
+from src.Emotion_Detection.utils.common import read_yaml, create_directories
 # Defining Paths basically we are mergging our constants from config.yaml with entity dataingestion entity
 class ConfigurationManager:
     def __init__( self,config_filepath = CONFIG_FILE_PATH,params_filepath = PARAMS_FILE_PATH):
@@ -26,3 +28,20 @@ class ConfigurationManager:
 # it will return all the paths 
         return data_ingestion_config   
       
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        return prepare_base_model_config
